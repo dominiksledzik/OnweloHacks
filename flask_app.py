1ff0b8c56f374bbb6451a98cef28aae64c6bcc90
Dominik Śledzik <57485073+dominiksledzik@users.noreply.github.com>
2023-09-30 21:59:01 +0200

    not working

--- flask_app.py
+++ flask_app.py
@@ -4,13 +4,23 @@ import json
 
 app = Flask(__name__,template_folder='./frontend/template')
 
+@app.route('/test/', methods = ['GET','POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form.get('user_input', '').split('\n')
+
+    else:
+        user_input = []
+   
+    return render_template('index.html',user_input=user_input)
+
 @app.route('/crypto/', methods=['GET'])
 def get_json():
     shortcuts = request.args.getlist('shortcuts')
     return CoginGeco.get_prices(*shortcuts)
 
 
-@app.route('/crypro_shortcuts/', methods=['GET'])
+@app.route('/crypto_shortcuts/', methods=['GET'])
 def get_crypto():
     with open('cryptocurrency.json', 'r') as json_file:
         return json.load(json_file)
