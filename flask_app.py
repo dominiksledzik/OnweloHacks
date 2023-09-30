2c5f17c165def91cfce6109c60ea00fd42feeaff
Jjzelll <jjzel1234@gmail.com>
2023-09-30 19:51:50 +0200

    flask uploaded

---
+++ flask_app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+from crypto_api_endpoints.cogingeco import CoginGeco
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_json():
+    shortcuts = request.args.getlist('shortcuts')
+    return CoginGeco.get_prices(*shortcuts)
+    
