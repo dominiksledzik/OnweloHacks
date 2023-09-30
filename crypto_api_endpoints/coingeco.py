ab1d1d38dad677310f15c7de8ef2b60b51ef8a66
Jjzelll <jjzel1234@gmail.com>
2023-09-30 18:35:07 +0200

    coingeco updated

--- crypto_api_endpoints/coingeco.py
+++ crypto_api_endpoints/coingeco.py
@@ -1,20 +1,31 @@
 import requests
 import json
 
+
 class CoginGeco:
+
 
     @staticmethod
     def get_prices(*shortcuts):
+
+        with open("./cryptocurrency.json", 'r') as json_file:
+            available_cryptocurrency = json.load(json_file)
+        
+        ids = [available_cryptocurrency[shortcut]['full_name'].lower() 
+               for shortcut in shortcuts]
+
         url = f"https://api.coingecko.com/api/v3/simple/price?ids="
-        for shortut in shortcuts:
-             url += f"{shortut},"
-        url = f"{url[:-1]}&vs_currencies=usd"
+        for id in ids:
+             url += f"{id},"
+        url = f"{url[:-1]}&vs_currencies=pln"
+
         headers = {'content-type': 'application/json'}
         response = requests.request("GET", url, headers=headers)
-        print(json.loads(response.text))
+
+        return json.loads(response.text)
 
     @staticmethod
     def conversion():
         pass
 
-CoginGeco.get_prices("zknftex", "bitcoin")
+CoginGeco.get_prices("BTC", "ETH")
