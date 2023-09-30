a095e747475b99ee51d899eff518d6fa79d20610
Dominik Śledzik <57485073+dominiksledzik@users.noreply.github.com>
2023-09-30 21:01:43 +0200

    jinja template for dropdown

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from crypto_api_endpoints.cogingeco import CoginGeco
-app = Flask(__name__)
+app = Flask(__name__,template_folder='./frontend/template')
 
 
 @app.route('/crypto/')
 def get_json():
     shortcuts = request.args.getlist('shortcuts')
     return CoginGeco.get_prices(*shortcuts)
-
-    
