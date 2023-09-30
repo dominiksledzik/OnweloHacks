11efc11c7c48971f84eae1c8dfa5e4751dd2c102
Jjzelll <jjzel1234@gmail.com>
2023-09-30 17:29:24 +0200

    CoginGeco with endpoint

--- crypto_api_endpoints/coingeco.py
+++ crypto_api_endpoints/coingeco.py
@@ -1,7 +1,20 @@
 import requests
+import json
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-headers = {'content-type': 'application/json'}
-response = requests.request("GET", url, headers=headers)
+class CoginGeco:
 
-print(response.text)
+    @staticmethod
+    def get_prices(*shortcuts):
+        url = f"https://api.coingecko.com/api/v3/simple/price?ids="
+        for shortut in shortcuts:
+             url += f"{shortut},"
+        url = f"{url[:-1]}&vs_currencies=usd"
+        headers = {'content-type': 'application/json'}
+        response = requests.request("GET", url, headers=headers)
+        print(json.loads(response.text))
+
+    @staticmethod
+    def conversion():
+        pass
+
+CoginGeco.get_prices("zknftex", "bitcoin")
